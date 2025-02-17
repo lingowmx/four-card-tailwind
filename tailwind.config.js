0610c6870b01e5b5093a2cae758dd3b277f4e9d3
lingowmx <clingow@hotmail.com>
2025-02-17 18:15:11 +0100

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ export default {
     extend: {
       fontFamily:{
         poppins:["Poppins", "sans-serif"]
+      },
+      colors:{
+        red: 'hsl(0, 78%, 62%)',
+        cyan: 'hsl(180, 62%, 55%)',
+        orange: 'hsl(34, 97%, 64%)',
+        blue: 'hsl(212, 86%, 64%)'
       }
     },
   },
